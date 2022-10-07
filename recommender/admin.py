4fd2eb9e9204a870b993f10f8e32a82a6c31fee6
mustaroe9 <43862218+mustaroe9@users.noreply.github.com>
2022-10-06 23:45:46 -0400

    Update admin.py
    Some changes to better display data on the admin pages about the musicdata

--- recommender/admin.py
+++ recommender/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import Musicdata
 
 # Register your models here.
-admin.site.register(Musicdata)
+@admin.register(Musicdata)
+class MusicAdmin(admin.ModelAdmin):
+    list_display = ['track_name', 'track_album_name', 'track_artist', 'duration_ms']
 
