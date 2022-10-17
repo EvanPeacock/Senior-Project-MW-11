8090db62519c0bce33cb98cac5b2cae0584e39fd
Evan Peacock <36444106+EvanPeacock@users.noreply.github.com>
2022-10-17 11:16:24 -0400

    Revert "Revert "Jackson individual project merge" oops"

--- recommender/models.py
+++ recommender/models.py
@@ -1,4 +1,8 @@
+from email.policy import default
+from enum import unique
+from unittest.util import _MAX_LENGTH
 from django.db import models  
+from django.contrib.auth.models import User
 
 class Musicdata(models.Model):
     track_id = models.TextField()
@@ -8,10 +12,10 @@ class Musicdata(models.Model):
     track_album_id  = models.TextField()
     track_album_name = models.TextField()
     track_album_release_date = models.IntegerField()
-    playlist_name = models.TextField()
-    playlist_id = models.TextField()
-    playlist_genre = models.TextField()
-    playlist_subgenre = models.TextField()
+    # playlist_name = models.TextField()
+    # playlist_id = models.TextField()
+    # playlist_genre = models.TextField()
+    # playlist_subgenre = models.TextField()
     danceability = models.FloatField()
     energy = models.FloatField()
     key = models.FloatField()
@@ -25,12 +29,11 @@ class Musicdata(models.Model):
     tempo = models.FloatField()
     duration_ms = models.IntegerField()
     
-# class User(models.Model):
-# #     username = models.CharField(max_length=25, Required=True)
-#     username = models.CharField(max_length=100, null=True)
-#     user_fname = models.CharField(max_length=50, null=False)
-#     user_lname = models.CharField(max_length=50, null   =False)
-#     user_email = models.EmailField(null=False)
-#     user_password = models.CharField(max_length=50, null=False)
-# #     liked_songs = models.ManyToManyField('Musicdata', blank=True)
-#     objects = UserManager
+class Playlist(models.Model):
+    playlist_name = models.CharField(max_length=50, null=False, blank=False, default='New Playlist')
+    playlist_id = models.IntegerField(unique=True, null=False)
+    playlist_owner = models.ManyToManyField(User, blank=True)
+    playlist_songs = models.ManyToManyField('Musicdata', blank=True)
+    
+    def creator(self):
+        return str(self.playlist_owner.all())
