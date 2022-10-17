116ed6d4292164375f5cc3318d0ae88b27e99836
Evan Peacock <36444106+EvanPeacock@users.noreply.github.com>
2022-10-17 11:18:26 -0400

    Revert "REVERT "Revert "Jackson individual project merge" oops""

--- recommender/admin.py
+++ recommender/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Musicdata, Playlist
+from .models import Musicdata
 
 # Register your models here.
 admin.site.register(Musicdata)
 
-admin.site.register(Playlist)
