935764b26bca956e8d676aa7cc7a986d73c5c9cc
Jrav99 <ravell55@students.rowan.edu>
2022-10-19 16:01:43 -0400

    Added playlists on myprofile page
    User playlists now appear on the profile page as well as option to add songs to playlists is there but non-functional so far

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jrav9\OneDrive\Documents\Fall 2022\Senior Project\IndividualProject\Bootstrap1\Senior-Project-MW-11-main\Senior-Project-MW-11-main\venv
+set VIRTUAL_ENV=C:\Users\jrav9\OneDrive\Documents\Fall 2022\Senior Project\Project\Senior-Project-MW-11\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
